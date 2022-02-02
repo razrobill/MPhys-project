8142e1aef9712d8ff9d5efe923acaa12c2ac7ca9
Talia Blain <tb1126@york.ac.uk>
2022-02-02 15:20:53 +0000

    changed the DH parameters again

--- valuesfromtable.py
+++ valuesfromtable.py
@@ -20,18 +20,18 @@ a6 = 0
 d1 = 1045
 d2 = 0
 d3 = 0
-d4 = 1025
-d5 = 290
-d6 = 0
+d4 = 720
+d5 = 0
+d6 = 290
 
 theta1 = 0
-theta2 = 0
+theta2 = 90*(pi/180)
 theta3 = 0
 theta4 = 0
 theta5 = 0
 theta6 = 0
 
-alpha1 = -90*(pi/180)
+alpha1 = 90*(pi/180)
 alpha2 = 0*(pi/180)
 alpha3 = 90*(pi/180)
 alpha4 = -90*(pi/180)
@@ -65,10 +65,10 @@ d_h_table_0_1 = np.array([[cos(theta1), -sin(theta1)*cos(alpha1), sin(theta1)*si
 transform_0_1 = d_h_table_0_1
 last_column1 = transform_0_1[:, 3]
 p1 = np.delete(last_column1, 3, 0)
-#print(p1)
-p1x = np.unique(p1[0])
-p1y = np.unique(p1[1])
-p1z = np.unique(p1[2])
+print(p1)
+p1x = np.unique(p1[0]) +p0x
+p1y = np.unique(p1[1]) +p0y
+p1z = np.unique(p1[2]) +p0z
 
 
 
@@ -77,14 +77,14 @@ d_h_table_1_2 = np.array([[cos(theta2), -sin(theta2)*cos(alpha2), sin(theta2)*si
                         [0, sin(alpha2), cos(alpha2), d2],
                         [0, 0, 0, 1]])
 
-transform_1_2 = d_h_table_1_2
+transform_1_2 = d_h_table_0_1 @ d_h_table_1_2
 last_column2 = transform_1_2[:, 3]
 p2 = np.delete(last_column2, 3, 0)
-p2x = p1x
-#print(p2x)
+print(p2)
+p2x = np.unique(p2[0])
 p2y = np.unique(p2[1])
-p2z = np.unique(p2[2]) +p1z + np.unique(p2[0])
-#print(p2z)
+p2z = np.unique(p2[2])
+
 
 
 d_h_table_2_3 = np.array([[cos(theta3), -sin(theta3)*cos(alpha3), sin(theta3)*sin(alpha3), a3*cos(theta3)],
@@ -92,44 +92,50 @@ d_h_table_2_3 = np.array([[cos(theta3), -sin(theta3)*cos(alpha3), sin(theta3)*si
                           [0, sin(alpha3), cos(alpha3), d3],
                           [0, 0, 0, 1]])
 
-transform_2_3 = d_h_table_2_3
+transform_2_3 = d_h_table_2_3 @ d_h_table_1_2 @ d_h_table_0_1
 last_column3 = transform_2_3[:, 3]
 p3 = np.delete(last_column3, 3, 0)
-p3x = p1x + 710
-#print(p3x)
+print(p3)
+p3x = np.unique(p3[0])
 p3y = np.unique(p3[1])
-p3z = p2z - np.unique(p3[0])
-#print(p3z)
+p3z = np.unique(p3[2])
+
 
 d_h_table_3_4 = np.array([[cos(theta4), -sin(theta4)*cos(alpha4), sin(theta4)*sin(alpha4), a4*cos(theta4)],
                           [sin(theta4), cos(theta4)*cos(alpha4), -cos(theta4)*sin(alpha4), a4*sin(theta4)],
                           [0, sin(alpha4), cos(alpha4), d4],
                           [0, 0, 0, 1]])
 
-transform_3_4 = d_h_table_3_4
+transform_3_4 = d_h_table_3_4 @ d_h_table_2_3 @ d_h_table_1_2 @ d_h_table_0_1
 last_column4 = transform_3_4[:, 3]
 p4 = np.delete(last_column4, 3, 0)
-p4x = p2x + np.unique(p4[2])
-print(p4x)
+print(p4)
+p4x = np.unique(p4[0])
 p4y = np.unique(p4[1])
-p4z = p3z
+p4z = np.unique(p4[2])
 
 d_h_table_4_5 = np.array([[cos(theta5), -sin(theta5)*cos(alpha5), sin(theta5)*sin(alpha5), a5*cos(theta5)],
                           [sin(theta5), cos(theta5)*cos(alpha5), -cos(theta5)*sin(alpha5), a5*sin(theta5)],
                           [0, sin(alpha5), cos(alpha5), d5],
                           [0, 0, 0, 1]])
 
-transform_4_5 = d_h_table_4_5
+transform_4_5 = d_h_table_4_5 @ d_h_table_3_4 @ d_h_table_2_3 @ d_h_table_1_2 @ d_h_table_0_1
 last_column5 = transform_4_5[:, 3]
 p5 = np.delete(last_column5, 3, 0)
-p5x = p4x + np.unique(p5[2])
+print(p5)
+p5x = np.unique(p5[0])
 p5y = np.unique(p5[1])
-p5z = p4z
+p5z = np.unique(p5[2])
 
 d_h_table_5_6 = np.array([[cos(theta6), -sin(theta6)*cos(alpha6), sin(theta6)*sin(alpha6), a6*cos(theta6)],
                           [sin(theta6), cos(theta6)*cos(alpha6), -cos(theta6)*sin(alpha6), a6*sin(theta6)],
                           [0, sin(alpha6), cos(alpha6), d6],
                           [0, 0, 0, 1]])
+
+transform_5_6 = d_h_table_5_6 @ d_h_table_4_5 @ d_h_table_3_4 @ d_h_table_2_3 @d_h_table_1_2 @ d_h_table_0_1
+last_column6 = transform_5_6[:, 3]
+p6 = np.delete(last_column6, 3, 0)
+print(p6)
 
 #multiplying to find transformation from frame 0 to 6
 #(@ symbol used for matrix multiplication)
@@ -137,7 +143,7 @@ d_h_table_5_6 = np.array([[cos(theta6), -sin(theta6)*cos(alpha6), sin(theta6)*si
 transform_0_6 = d_h_table_0_1 @ d_h_table_1_2 @ d_h_table_2_3 @ d_h_table_3_4 @ d_h_table_4_5 @ d_h_table_5_6
 
 print("Homogeneous Matrix from frame 0 to frame 6:  ")
-#print(transform_0_6)
+print(transform_0_6)
 
 #the final T vector contains the position of the end effector, the R matrix contains the orientation
 #of the end effector
@@ -181,4 +187,3 @@ ax.plot3D(p5x, p5y, p5z, 'blue', marker="o")
 #showing plot
 matplotlib.pyplot.show()
 
-#trying to extract values from tables

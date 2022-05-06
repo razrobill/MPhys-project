ce89286f690bd2e7aadba0670221076b4cfc06b9
razrobill <tb1126@york.ac.uk>
2022-05-06 15:18:50 +0100

    improving inverse kinematics plots

--- forwardkinematicsfinal.py
+++ forwardkinematicsfinal.py
@@ -250,15 +250,15 @@ if __name__ == '__main__':
 
     kr360 = kr360()
 
-    endeffector = kr360.forward_kinematics(0, 90, 1, 0, 0, 0)
+    endeffector = kr360.forward_kinematics(0, 90, 0, 0, 0, 0)
     print('end effector position: ')
     print(endeffector)
     last_column = endeffector[:, 3]
     end_effector_position = np.delete(last_column, 3, 0)
-    print(end_effector_position)
-    jointcoordinates = kr360.find_joint_positions(0, 90, 1, 0, 0, 0)
+    #print(end_effector_position)
+    jointcoordinates = kr360.find_joint_positions(0, 90, 0, 0, 0, 0)
     #validity2 = kr360.valid_theta_configurations(2, 200, 'angle')
-    all_theta_configurations = [0, 90, 1, 0, 0, 0]
+    all_theta_configurations = [0, 90, 0, 0, 0, 0]
     check = kr360.check_configuration(all_theta_configurations)
     #print(check)
 
--- inversejacobian.py
+++ inversejacobian.py
@@ -10,6 +10,9 @@ from sympy import Matrix, Symbol, symbols, solveset, solve, simplify, diff, det
 from sympy import S, erf, log, sqrt, pi, sin, cos, tan
 from sympy import init_printing
 
+plt.rcParams["font.family"] = 'Times New Roman'
+plt.rcParams["figure.autolayout"] = True
+
 #d-h parameters in mm
 a1 = 500
 a2 = 1300
@@ -187,7 +190,7 @@ print(p_i)
 
 #final (target) point of the end effector, defined as a relative movement from the initial position, for example moving
 #the arm down in the z-axis by 5cm
-p_f = p_i + Matrix([0, 0, 1])
+p_f = p_i + Matrix([0, 0, 500])
 
 dp = p_f - p_i
 
@@ -195,7 +198,7 @@ dp_threshold = 5
 dp_step = 1
 theta_max_step = 0.5
 j = 0
-max_steps = 50
+max_steps = 500
 
 while dp.norm() > dp_threshold and j < max_steps:
     print(f'step{j}: θ[{theta_i}, P[{p_i}]')
@@ -228,6 +231,7 @@ while dp.norm() > dp_threshold and j < max_steps:
     p6sub = p6.subs({theta1: theta_i[0], theta2: theta_i[1], theta3: theta_i[2], theta4: theta_i[3], theta5: theta_i[4],
                      theta6: theta_i[5]}).evalf()
     soa = np.array([p0sub, p1sub, p2sub, p3sub, p4sub, p5sub, p6sub], dtype=object)
+    soa2 =np.array([p6sub])
     X, Y, Z, = zip(*soa)
     X = np.array(X)
     Y = np.array(Y)
@@ -235,17 +239,33 @@ while dp.norm() > dp_threshold and j < max_steps:
     X = np.ndarray.flatten(X)
     Y = np.ndarray.flatten(Y)
     Z = np.ndarray.flatten(Z)
+    X2, Y2, Z2, = zip(*soa2)
+    X2 = np.array(X2)
+    Y2 = np.array(Y2)
+    Z2 = np.array(Z2)
+    X2 = np.ndarray.flatten(X2)
+    Y2 = np.ndarray.flatten(Y2)
+    Z2 = np.ndarray.flatten(Z2)
     fig = matplotlib.pyplot.figure(1)
     ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    ax.set_xlabel("X (mm)")
+    ax.set_ylabel("Y (mm)")
+    ax.set_zlabel("Z (mm)")
+    matplotlib.pyplot.xticks(fontsize=9)
     ax.set_xlim([-2000, 2000])
     ax.set_ylim([0, 2000])
     ax.set_zlim([0, 3000])
-    # ax.view_init(elev=45, azim=45)
+    #ax.view_init(elev=30, azim=-60)
+    #ax.view_init(elev=45, azim=45)
     ax.view_init(elev=10, azim=90)
-    ax.plot3D(X, Y, Z, 'blue', marker="o")
+    ax.plot3D(X2, Y2, Z2, 'red', marker="^", zorder=4)
+    ax.scatter(X2, Y2, Z2, color='red', marker='^', label="End effector position", zorder=1)
+    ax.plot3D(X, Y, Z, 'darkorange', marker="o", zorder=3)
+    ax.scatter(X, Y, Z, color='darkorange', marker="o", label="Joint positions", zorder=2)
+    plt.title("Initial Configuration of KUKA KR 360, with End Effector "
+              "\n moved 500mm upwards through Inverse Kinematics")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper right")
+    plt.savefig('inverse_plot3.png')
     matplotlib.pyplot.draw()
     matplotlib.pyplot.show()
     matplotlib.pyplot.pause(0.1)
